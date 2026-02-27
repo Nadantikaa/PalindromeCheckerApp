2349490498751fdc5c70d65e11ad52d387cad3f2
Nadantikaa <nadantikaa@gmail.com>
2026-02-27 16:14:13 +0530

    uc5 Stack palindrome validator

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,34 +1,28 @@
 /**
- * Use case:1 Character Array based Validation
+ * Use case:Stack based palindrome checker
  *
  *
- *This class validates a palindrome by converting the string
- * into a character array and comparing characters using the
- * two pointer technique,
+ *This class validates a palindrome using a stack data structure
+ * which follows the LIFO principle
  *
  * @author Developer
  * @version 1.0
  */
 public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Input Text:");
-    String input = scanner.nextLine();
-    char[] inputarray =new char[input.length()];
+    String input= "noon";
+    Stack<Character> stack=new Stack<>();
     boolean IfPalindrome=true;
-    for(int i=0;i<input.length()-1; i++){
-        inputarray[i]=input.charAt(i);
+    for(char c : input.toCharArray()){
+        stack.push(c);
+
     }
 
-    int right=input.length()-1;
-    for(int left=0;left<input.length()/2; left++){
-        if(input.charAt(left)!=input.charAt(right)){
+    for(char c: input.toCharArray()){
+        if(stack.pop()!=c){
             IfPalindrome=false;
-            break;
         }
-        right--;
-
-
     }
+
     if(IfPalindrome){
         System.out.println("Is it a Palindrome:True");
     }else{
