58605e32b14c5fbf8f22febb0b76ed4d000b96cf
nadantikaa <nadantikaa@gmail.com>
2026-03-09 14:40:37 +0530

    UC9: Recursive call

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,32 +1,33 @@
 /**
- * Use case:deque based optimized palindrome Check
+ * Use case9:Recursive Palindrome Checker
  *
  *
- *This class validates a palindrome using a deque.
+ *This class validates a palindrome using recursion.
  *
  * @author Developer
  * @version 1.0
  */
-public static void main(String[] args){
-    String input= "refer";
-    Deque<Character> deque=new ArrayDeque<>();
-    boolean IfPalindrome=true;
+public class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        String input = "madam";
+        int start = 0;
+        int end = input.length()-1;
+        boolean IfPalindrome = true;
 
-    for(char c : input.toCharArray()){
-        deque.add(c);
-
-    }
-
-    while(deque.size()>1){
-        if(deque.peek()!=deque.poll()){
-            IfPalindrome=false;
+        System.out.println(" Input string:" + input);
+        if (IfPalindrome) {
+            System.out.println("Is it a Palindrome:True");
+        } else {
+            System.out.println("Is it a Palindrome:False");
         }
     }
-    System.out.println(" Input string:"+input);
-    if(IfPalindrome){
-        System.out.println("Is it a Palindrome:True");
-    }else{
-        System.out.println("Is it a Palindrome:False");
+    private static boolean check (String input,int start, int end){
+        if(start>=end){
+            return true;
+        }
+        if (input.charAt(start) != input.charAt(end)) {
+            return false;
+        }
+        return check(input,start++,end--);
     }
-
 }
