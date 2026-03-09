0929f1ea1a848791fd7ff35c68b5cbb4a991a1cd
nadantikaa <nadantikaa@gmail.com>
2026-03-09 14:56:35 +0530

    UC10: Normalized string

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,21 @@
 /**
- * Use case 8:linked list Based Palindrome Checker
+ * Use case 10: Normalized Palindrome Vaidation
  *
  *
- *This class validates a palindrome using a linkedlist.
+ *This class validates a palindrome after preprocessing the input string.
  *
  * @author Developer
  * @version 1.0
  */
 public static void main(String[] args){
-    String input= "level";
-    LinkedList<Character> list=new LinkedList<>();
-    LinkedList<Character> reverse=new LinkedList<>();
+    String input= "A man a plan a canal Panana";
+    String normalized=input.toLowerCase().replace("[^a-z]","");
     boolean IfPalindrome=true;
 
-    for(char c : input.toCharArray()){
-        list.add(c);
-
-    }
-    int mid=list.size()/2;
-    LinkedList<Character> secondhalf= new LinkedList<>(list.subList(mid,list.size()));
-    Collections.reverse(secondhalf);
-    while(secondhalf.size()>1){
-        if(list.removeFirst()!=secondhalf.removeFirst()){
+    for(int i=0;i<normalized.length()/2;i++){
+        if (normalized.charAt(i)!=normalized.charAt(normalized.length()-1-i)){
             IfPalindrome=false;
+            break;
         }
     }
     System.out.println("Input string:"+input);
